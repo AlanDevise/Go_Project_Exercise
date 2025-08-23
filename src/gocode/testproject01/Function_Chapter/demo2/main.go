980bf637f01c4d906059d07a3cfe3c3cd3d09878
Alan Zhang <initiator@alandevise.com>
2025-08-23 16:51:46 +0800

    Add initial implementation of main.go with basic variable declarations and print statements

---
+++ src/gocode/testproject01/Function_Chapter/demo2/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = 10
+	fmt.Println("main函数开始执行", name)
+	var xiaoshu = 12.34
+	fmt.Println("xiaoshu=", xiaoshu)
+	fmt.Printf("xiaoshu的类型是%T\n", xiaoshu)
+	var isOk = true
+	fmt.Println("isOk=", isOk)
+	var b = 10
+	fmt.Println("b=", b)
+	fmt.Printf("b的类型是%T\n", b)
+
+	var variable1 int
+	fmt.Println("variable1=", variable1)
+}
